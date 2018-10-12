618c703a1164653855effae5ca21772cd40607a6
McNuggets7778 <mockvich88@gmail.com>
2018-10-12 19:07:44 +0300

    site

---
+++ js/script.js
@@ -0,0 +1,27 @@
+console.log("Возможно, сделали не так, как надо было");
+
+var a=7,b=15,c=22,sum1,sum2,sum3;
+if(a>b)
+sum1=a+b
+	{
+		if(sum1<c)
+			console.log("Yes1");
+		else
+			console.log("No1");
+	}	
+if(a<b)
+sum2=a-b;
+    {
+		if(sum2<c)
+			console.log("Yes2");
+		else
+			console.log("No2");
+	}
+if(a=b)
+sum3=a*b;
+	{
+	    if(sum3<c)
+			console.log("Yes3")
+		else
+			console.log("No3")
+	}
